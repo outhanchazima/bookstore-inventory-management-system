eaadb8536bf3841913316d5b70101d2320f49fd1
outhanchazima <outhan254@gmail.com>
2022-02-07 15:16:40 +0300

    pytest-  user unit tests and github action workflow

--- manage.py
+++ manage.py
@@ -8,5 +8,15 @@ app.app_context().push()
 
 cli = FlaskGroup(app)
 
+@cli.command("test")
+def unitTesting():
+    """Runs the unit tests."""
+    import unittest
+    tests = unittest.TestLoader().discover("app/tests", pattern="test*.py")
+    result = unittest.TextTestRunner(verbosity=2).run(tests)
+    if result.wasSuccessful():
+        return 0
+    return 1
+
 if __name__ == '__main__':
     cli()
