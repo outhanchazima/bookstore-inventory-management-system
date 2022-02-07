92dd2d0a856833100af7c36bdd749d0a3c3b5942
outhanchazima <outhan254@gmail.com>
2022-02-07 16:14:40 +0300

    git created realtionship with books table, refactored table name to Author, removed unncesarry query function find by botname

--- app/src/models/author.py
+++ app/src/models/author.py
@@ -1,14 +1,17 @@
 from datetime import datetime
 from app.src import db
 
-class Authors(db.Model):
-    __tablename__ = 'authors'
+class Author(db.Model):
+    __tablename__ = 'author'
 
     id: int = db.Column(db.Integer, primary_key=True, autoincrement=True)
     first_name: str = db.Column(db.String(64), nullable=False)
     last_name: str = db.Column(db.String(64), nullable=False)
     email: str = db.Column(db.String|(50), nullable=False, unique=True)
     date_of_birth: datetime = db.Column(db.DateTime, nullable=True)
+    
+    # one to many with Books table 
+    book = db.relationship('Book', backref='author', lazy=True)
 
     @classmethod
     def create(cls, **kw):
@@ -17,9 +20,5 @@ class Authors(db.Model):
         db.session.commit()
 
     @classmethod
-    def findByAutherID(cls, id: int) -> "Authors":
+    def findByAuthorID(cls, id: int) -> "Author":
         return cls.query.filter_by(id=id).first()
-
-    @classmethod 
-    def find_by_botname(cls, botname: str) -> "Authors":
-        return cls.query.filter_by(botname=botname).first()
