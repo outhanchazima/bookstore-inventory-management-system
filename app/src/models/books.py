b684e882802b6371ee304a10378026ff8d6d98ad
outhanchazima <outhan254@gmail.com>
2022-02-07 16:05:18 +0300

    created books models

---
+++ app/src/models/books.py
@@ -0,0 +1,15 @@
+import datetime
+from app.src import db
+
+class Books(db.Model):
+    __tablename__ = 'books'
+
+    id: int = db.Column(db.Integer, primary_key=True)
+    title: str = db.Column(db.String(100), nullable=False)
+    year_published: datetime = db.Column(db.Datetime, nullable=False)
+    isbn_no = db.Column(db.String(100), nullable=False)
+    description: str = db.Column(db.String(1000), nullable=False)
+
+    # relatiionship with authors
+    author_id: int = db.Column(db.Integer, db.ForeignKey('authors.id'), nullable=False)
+   
