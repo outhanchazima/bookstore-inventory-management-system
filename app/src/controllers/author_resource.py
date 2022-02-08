ae44820b702bb8f94d471d1b73ef95085aa0aea4
outhanchazima <outhan254@gmail.com>
2022-02-08 06:41:33 +0300

    create new author

--- app/src/controllers/author_resource.py
+++ app/src/controllers/author_resource.py
@@ -1,8 +1,10 @@
+from flask import request
 from flask_restx import Resource
 
 from app.src.dto.author import AuthorDto
 from app.src.utils.decorators import tokenRequired
 from app.src.services.authors.authors_service import saveNewAuthor, getAllAuthors, getAnAuthor
+from typing import Dict, Tuple
 
 api = AuthorDto.api
 author = AuthorDto.author
@@ -11,7 +13,15 @@ author = AuthorDto.author
 class AuthorResource(Resource):
     @api.doc('List all available authors')
     # @tokenRequired
-    @api.marshal_list_with(author, envelope='data')
     def get(self):
         """List all available authors"""
         return getAllAuthors()
+
+    @api.expect(author, validate=True)
+    @api.response(201, 'Author successfully created.')
+    # @tokenRequired
+    @api.marshal_with(author)
+    def post(self) -> Tuple[Dict[str, str], int]:
+        """Create a new author"""
+        data = request.json
+        return saveNewAuthor(data)
--- app/src/services/authors/authors_service.py
+++ app/src/services/authors/authors_service.py
@@ -1,18 +1,26 @@
 from flask import jsonify
 from app.src.models.author import Author
+from app.src.services import authors
 from app.src.utils.base_model import serializerDB
 from app.src.utils.logger import Logger
+from app.src import db
+from typing import Dict, Tuple
+import json
 
 logger = Logger().getLogger()
 
-def getAllAuthors():
-    try:
-        authors = Author.getAllAuthors()
-    except Exception as e:
-        return {"status": "fail", "message":str(e)}, 400
+def getAllAuthors()-> Tuple[Dict[str, str], int]:
+    authors = db.session.query(Author).all()
     logger.debug(f"Authors from the database {authors}")
     if authors:
-        AuthorsList = serializerDB(jsonify(authors))
+        AuthorsList = [{
+            "id": author.id,
+            "first_name": author.first_name,
+            "last_name": author.last_name,
+            "date_of_birth": author.date_of_birth.strftime("%d-%b-%Y (%H:%M:%S.%f)"),
+            "created_on": author.created_on.strftime("%d-%b-%Y (%H:%M:%S.%f)")
+        } for author in authors ]
+        logger.debug(f"Authors from the database {authors}")
         responseObject = {
             "status": "success",
             "message": AuthorsList
@@ -25,8 +33,22 @@ def getAllAuthors():
         }
         return responseObject, 200
   
-def saveNewAuthor():
-    pass
+def saveNewAuthor(data: Dict[str, str]) -> Tuple[Dict[str, str], int]:
+    # Check if author already exists
+    author = db.session.query(Author).filter_by(email = data["email"]).first()
+    if not author:
+        newAuthor = Author(
+            first_name = data["first_name"],
+            last_name = data["last_name"],
+            email = data["email"],
+            date_of_birth = data["date_of_birth"]
+        )
+        db.session.add(newAuthor)
+        db.session.commit()
+
+        return {"status":"success","message": "Author created successfully"}, 201
+    else:
+        return {"status":"fail","message":"An author with that email already exists"}, 409
 
 def getAnAuthor():
     pass
